2b4780293f1deac1fc60d88ac840bdce9a473373
ALUNO SESI PAULISTA <aluno.sesipaulista@SISTEMAFIEPE.LOCAL>
2024-03-15 15:51:57 -0300

    commit da classe aluno

---
+++ classe_Aluno.py
@@ -0,0 +1,11 @@
+class Aluno(Pessoa):
+    def __init__(self, matricula, nome, idade):
+        self.matricula = matricula
+        self.nome = nome
+        self.idade = idade
+
+    def info(self):
+        print("As informações é: " )
+        print('Seu nome: ', self.nome)
+        print('Sua idade é: ', self.idade)
+        print('Sua matricula é: ', self.matricula)
